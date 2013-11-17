aec9fefaea7fbda31274744fb030309c59ac5ea2
namibj <namibj@namibj-PC.fritz.box>
2013-11-17 21:59:35 +0100

    Schnell commit. Unfertig.

--- Projekt42/src/projekt42/Gegenstand.java
+++ Projekt42/src/projekt42/Gegenstand.java
@@ -3,11 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package projekt42;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 /**
  *
@@ -16,39 +16,55 @@ import javafx.scene.image.ImageView;
 public enum Gegenstand {
 
     /**
-     *Ein Demo-Dings, nur um die Struktur für das Einfügen zu zeigen.
+     * Ein Demo-Dings, nur um die Struktur für das Einfügen zu zeigen.
      */
-    KOERPER_1("Körper","Ein Körper.",""),
-
+    KOERPER_1("Körper", "Ein Körper.", "", false),
     /**
-     *Noch nen Demo-Dings.
+     * Noch nen Demo-Dings.
      */
-    LEICHE_1("","",""),
-
+    LEICHE_1("", "", "", false),
     /**
-     *Das dritte Demo-Dings.
+     * Das dritte Demo-Dings.
      */
-    SCHLUESSEL_1("Schlüssel","Ein Schlüssel.", "key.png");
+    SCHLUESSEL_1("Schlüssel", "Ein Schlüssel.", "key.png", true, "key");
     Image defaultImage;
-    String name,tip;
-    Gegenstand(String Name, String tooltip, String pathDefaultImg){
-        defaultImage = new Image(Gegenstand.class.getResource("images/"+pathDefaultImg).toString(), false);
-        tip=tooltip;
-        name=Name;
-    }
+    Image[] roomImages;
+    String name, tip;
+private final boolean toTakeAway;
     
-    public ImageView getImageView(){
-        ImageView imgView = new ImageView(defaultImage);
-        
-        return new ImageView(defaultImage);
+    Gegenstand(String Name, String tooltip, String pathDefaultImg, boolean ToTakeAway, String... pathsRoomImages) {
+        defaultImage = new Image(Gegenstand.class.getResource("images/" + pathDefaultImg).toString(), false);
+        for (int i = 0; 1 < pathsRoomImages.length; i++) {
+            roomImages[i] = new Image(Gegenstand.class.getResource("images/" + pathsRoomImages + i + ".png").toString(), false);
+        }
+        tip = tooltip;
+        name = Name;
+        toTakeAway = ToTakeAway;
+
     }
-    
-    public String getToolTip(){
+
+    public ImageView getImageView(int version) {
+        ImageView imgView;
+        if (version < 0) {
+            imgView = new ImageView(defaultImage);
+            imgView.setOnDragDetected((event) -> imgView.startFullDrag());
+            imgView.setUserData(true);
+        } else {
+            imgView = new ImageView(roomImages[version]);
+            if(toTakeAway)
+                imgView.setOnDragDetected((event) -> imgView.startFullDrag());
+            imgView.setUserData(true);
+        }
+        imgView.set
+        return imgView;
+    }
+
+    public String getToolTip() {
         return tip;
     }
-    
-    public String getName(){
+
+    public String getName() {
         return name;
     }
-    
+
 }
